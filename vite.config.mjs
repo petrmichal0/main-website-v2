7fd5c0c74b72ac28dc025918cbc2ab1d13cd626a
Petr Michal <122700272+petrmichal0@users.noreply.github.com>
2024-10-09 07:45:42 +0200

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build", // umístění produkčního buildu
+    outDir: "dist", // umístění produkčního buildu
   },
 });
